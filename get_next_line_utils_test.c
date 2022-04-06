9cf16a7d2e1fe019696c672adebabfc651aa2616
Seonghyeon Choe <schoe@c2r4s2.42seoul.kr>
2022-04-06 22:00:07 +0900

    fdfadfadf

---
+++ get_next_line_utils_test.c
@@ -0,0 +1,65 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line_utils_bonus.c                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: schoe <schoe@student.42seoul.kr>           +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/04/01 11:29:52 by schoe             #+#    #+#             */
+/*   Updated: 2022/04/06 21:58:53 by schoe            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+#include "get_next_line.h"
+
+int	ft_strlen(char *str)
+{
+	int	i;
+
+	i = 0;
+	while (str[i])
+		i++;
+	return (i);
+}
+
+char	*ft_strjoin(char *s1, char *s2, size_t s2_len)
+{
+	char	*temp;
+	size_t	s1_len;
+	size_t	i;
+	size_t	k;
+
+	i = 0;
+	k = 0;
+	if (s1 == NULL)
+		return (ft_strdup(s2, s2_len));
+	s1_len = ft_strlen(s1);
+	temp = (char *)malloc(sizeof(char) * (s1_len + s2_len + 1));
+	if (temp == NULL)
+		return (NULL);
+	while (i < s1_len)
+		temp[i++] = s1[k++];
+	k = 0;
+	while (i < s1_len + s2_len)
+		temp[i++] = s2[k++];
+	temp[i] = '\0';
+	free(s1);
+	return (temp);
+}
+
+char	*ft_strdup(char *s1, size_t size)
+{
+	size_t	i;
+	char	*s2;
+
+	i = 0;
+	s2 = (char *)malloc(sizeof(char) * (size + 1));
+	if (s2 == NULL)
+		return (NULL);
+	while (i < size)
+	{
+		s2[i] = s1[i];
+		i++;
+	}
+	s2[i] = '\0';
+	return (s2);
+}
